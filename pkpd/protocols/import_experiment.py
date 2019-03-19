db09fc86f5f039d515ad539da6e6b2bad5577c32
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-03-19 22:42:55 +0100

    * Add protocols

---
+++ pkpd/protocols/import_experiment.py
@@ -0,0 +1,97 @@
+# **************************************************************************
+# *
+# * Authors:     Carlos Oscar Sorzano (coss@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'coss@cnb.csic.es'
+# *
+# **************************************************************************
+
+"""
+Import experiment
+"""
+from base import ProtImportFiles
+import pyworkflow.protocol.params as params
+from pyworkflow.em.data import PKPDExperiment
+from os.path import exists, basename
+import sys
+from pyworkflow.utils.path import copyFile
+
+# TESTED in test_workflow_gabrielsson_pk01.py
+# TESTED in test_workflow_gabrielsson_pk02.py
+# TESTED in test_workflow_gabrielsson_pk03.py
+# TESTED in test_workflow_gabrielsson_pk04.py
+# TESTED in test_workflow_gabrielsson_pk05.py
+# TESTED in test_workflow_gabrielsson_pk06.py
+# TESTED in test_workflow_gabrielsson_pk07.py
+# TESTED in test_workflow_gabrielsson_pk08.py
+# TESTED in test_workflow_gabrielsson_pk09.py
+# TESTED in test_workflow_gabrielsson_pk10.py
+# TESTED in test_workflow_gabrielsson_pk11.py
+# TESTED in test_workflow_gabrielsson_pk12.py
+# TESTED in test_workflow_gabrielsson_pk13.py
+# TESTED in test_workflow_gabrielsson_pk14.py
+# TESTED in test_workflow_gabrielsson_pk15.py
+# TESTED in test_workflow_gabrielsson_pk16.py
+# TESTED in test_workflow_gabrielsson_pk17.py
+# TESTED in test_workflow_gabrielsson_pk18.py
+# TESTED in test_workflow_gabrielsson_pk19.py
+# TESTED in test_workflow_gabrielsson_pk20.py
+# TESTED in test_workflow_gabrielsson_pk25.py
+# TESTED in test_workflow_gabrielsson_pk39.py
+# TESTED in test_workflow_gabrielsson_pk43.py
+# TESTED in test_workflow_gabrielsson_pd03.py
+# TESTED in test_workflow_gabrielsson_pd11.py
+
+class ProtImportExperiment(ProtImportFiles):
+    """ Protocol to import an PKPD experiment\n
+        Protocol created by http://www.kinestatpharma.com\n"""
+    _label = 'import experiment'
+    IMPORT_FROM_FILES = 1 
+    
+    def __init__(self, **args):
+        ProtImportFiles.__init__(self, **args)
+
+    def _defineParams(self, form):
+        form.addSection(label='Input')
+        form.addParam('inputFile', params.PathParam,
+                      label="File path", allowsNull=False,
+                      help='Specify a path to desired experiment file.')
+         
+    def _insertAllSteps(self):
+        self._insertFunctionStep('createOutputStep', self.inputFile.get())
+        
+    def createOutputStep(self, inputPath):
+        localPath = self._getPath(basename(inputPath))
+        experiment = PKPDExperiment()
+        experiment.load(inputPath, verifyIntegrity=False)
+        experiment._printToStream(sys.stdout)
+        experiment.write(localPath)
+        self._defineOutputs(outputExperiment=experiment)
+
+    def _summary(self):
+        summary = ['Experiment imported from file: *%s*' % self.inputFile]
+        return summary
+    
+    def _validate(self):
+        errors = []
+        if (not exists(self.inputFile.get())):
+            errors.append("Experiment not found at *%s*" % self.inputFile.get())
+        return errors
---
+++ pkpd/protocols/protocol_pkpd.py
@@ -0,0 +1,102 @@
+# **************************************************************************
+# *
+# * Authors:     Carlos Oscar Sorzano (coss@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'coss@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+In this module are protocol base classes related to PKPD
+
+"""
+import sys
+import os
+from pyworkflow.em.protocol import *
+from pyworkflow.em.data import PKPDExperiment, PKPDFitting
+import pyworkflow.protocol.params as params
+
+class ProtPKPD(EMProtocol):
+    def printSection(self, msg):
+        print("**********************************************************************************************")
+        print("Section: %s"%msg)
+        print("**********************************************************************************************")
+
+    def readExperiment(self,fnIn, show=True):
+        experiment = PKPDExperiment()
+        experiment.load(fnIn)
+        if show:
+            self.printSection("Reading %s"%fnIn)
+            experiment._printToStream(sys.stdout)
+        return experiment
+
+    def writeExperiment(self, experiment, fnOut):
+        self.printSection("Writing %s"%fnOut)
+        experiment._printToStream(sys.stdout)
+        experiment.write(fnOut)
+
+    def readFitting(self, fnIn, show=True, cls=""):
+        fitting = PKPDFitting(cls)
+        fitting.load(fnIn)
+        if show:
+            self.printSection("Reading %s"%fnIn)
+            fitting._printToStream(sys.stdout)
+        return fitting
+
+    def doublePrint(self,fh,msg):
+        fh.write(msg+"\n")
+        print(msg)
+
+    def addFileContentToMessage(self,msg,fn):
+        if os.path.exists(fn):
+            fh = open(fn)
+            for line in fh.readlines():
+                msg.append(line.strip())
+            fh.close()
+
+    def loadInputExperiment(self):
+        """ If the protocol has an attribute 'inputExperiment',
+        load that experiment from file. If not, return None. """
+        experiment = self.getAttributeValue('inputExperiment', None)
+
+        if experiment:
+            experiment.load()
+            return experiment
+
+        return None
+
+    def setInputExperiment(self):
+        """ Set as self.experiment the experiment that is referenced
+        in the attribute self.inputExperiment.
+        """
+        self.setExperiment(self.loadInputExperiment())
+
+def addDoseToForm(form):
+    form.addParam('doses', params.TextParam, height=5, width=70, label="Doses", default="",
+                  help="Structure: [Dose Name] ; [via=ViaName] ; [doseType] ; [time description] ; [dose description]\n"\
+                       "The dose name should have no space or special character\n"\
+                       "The viaName must have been declared in the vias section"
+                       "Valid units are: h, mg, ug, ug/mL, ...\n"\
+                       "The description is either a bolus or an infusion as shown in the examples\n"\
+                       "For infusion the dose must be the amount per unit time, e.g., mg/min\n"\
+                       "Examples:\n"\
+                       "Infusion0 ; via=Intravenous; infusion; t=0.500000:0.750000 h; d=60*weight/1000 mg\n"\
+                       "Bolus1 ; via=Oral; bolus; t=2.000000 h; d=100 mg\n"\
+                       "Treatment ; via=Oral; repeated_bolus; t=0:8:48 h; d=100 mg")
