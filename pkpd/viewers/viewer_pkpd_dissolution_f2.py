0fd98b5aec1fa0d08d5f8213183324081c83ebc9
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2019-05-04 11:07:54 +0200

    Viewer for f2 added

--- pkpd/viewers/viewer_pkpd_dissolution_f2.py
+++ pkpd/viewers/viewer_pkpd_dissolution_f2.py
@@ -1,7 +1,6 @@
 # **************************************************************************
 # *
-# * Authors: Yunior C. Fonseca Reyna    (cfonseca@cnb.csic.es)
-# *
+# * Authors:     J.M. De la Rosa Trevin (delarosatrevin@gmail.com)
 # *
 # * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
 # *
@@ -21,14 +20,32 @@
 # * 02111-1307  USA
 # *
 # *  All comments concerning this program package may be sent to the
-# *  e-mail address 'scipion@cnb.csic.es'
+# *  e-mail address 'jmdelarosa@cnb.csic.es'
 # *
 # **************************************************************************
 
-from .viewer import (PKPDExperimentViewer, PKPDFittingViewer, PKPDCSVViewer,
-                     PKPDAnalysisViewer, PKPDStatisticsLabelViewer,
-                     PKPDRegressionLabelsViewer, PKPDPopulationViewer,
-                     PKPDAllometricScalingViewer)
-from .viewer_pkpd_dissolution_f2 import PKPDDissolutionF2Viewer
-from .viewer_pkpd_simulate_drug_interactions import PKPDSimulateDrugInteractionsViewer
-from .viewer_pkpd_simulate_liver_flow import PKPDSimulateLiverFlowViewer
+from numpy import genfromtxt
+
+from pyworkflow.viewer import Viewer, DESKTOP_TKINTER
+from pyworkflow.em.viewers.plotter import EmPlotter
+
+from pkpd.protocols import ProtPKPDDissolutionF2
+
+class PKPDDissolutionF2Viewer(Viewer):
+    _targets = [ProtPKPDDissolutionF2]
+    _environments = [DESKTOP_TKINTER]
+
+    def visualize(self, obj, **kwargs):
+        prot = obj
+        f1 = genfromtxt(prot._getExtraPath('f1.txt'))
+        f2 = genfromtxt(prot._getExtraPath('f2.txt'))
+
+        plotter = EmPlotter()
+        plotter.createSubPlot("Histogram of f1", "f1", "Count")
+        plotter.plotHist(f1, 50)
+        plotter.show()
+
+        plotter = EmPlotter()
+        plotter.createSubPlot("Histogram of f2", "f2", "Count")
+        plotter.plotHist(f2, 50)
+        plotter.show()
