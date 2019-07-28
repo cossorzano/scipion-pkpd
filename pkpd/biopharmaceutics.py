ad86ea97f712618744d3237cd90f9605dd49025d
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2019-07-28 12:22:39 +0200

    Bug fix in SplinesXY

--- pkpd/biopharmaceutics.py
+++ pkpd/biopharmaceutics.py
@@ -460,7 +460,7 @@ class BiopharmaceuticsModelSplineXYGeneric(BiopharmaceuticsModel):
         if t>=self.tmax or self.tmax<=0:
             return 0.0
         if self.parametersPrepared is None or not np.array_equal(self.parametersPrepared,self.parameters):
-            self.knots = np.append(np.insert(self.parameters[1::2],0,0),1)
+            self.knots = np.append(np.insert(self.parameters[1::2],0,0),1)*self.tmax
             self.knotsY = np.append(np.insert(self.parameters[2::2],0,0),1)
             self.knots=np.sort(self.knots)
             self.knotsY=np.sort(self.knotsY)
