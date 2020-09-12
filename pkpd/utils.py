f9470df0db4414b89bc952afe2012fe35c49f295
Carlos Oscar Sorzano <coss@cnb.csic.es>
2020-09-12 12:08:57 +0200

    Protection against weird conditions in find_nearest

--- pkpd/utils.py
+++ pkpd/utils.py
@@ -57,7 +57,7 @@ def parseRange(auxString):
 
 def find_nearest(array,value):
     idx = np.searchsorted(array, value, side="left")
-    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+    if idx > 0 and (idx >= len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
         return idx-1
     else:
         return idx
