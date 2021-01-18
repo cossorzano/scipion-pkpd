52f78b872262a4cb5b972b90719542d60061a03a
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2021-01-18 15:42:53 +0100

    * Include data/tests into setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     keywords='pkpd',  # Optional
     packages=find_packages(),
     package_data={  # Optional
-       'pkpd': ['protocols.conf', 'pkpd/data'],
+       'pkpd': ['protocols.conf', 'data/*.*'],
     },
     project_urls={  # Optional
         'Bug Reports': 'https://github.com/cossorzano/scipion-pkpd/issues',
